a6835d715d1e91db4b616e0f416eee8eff862e3f
Roberto J. Vargas <rvargas@rv-solutions.net>
2025-05-18 15:03:00 -0500

    feat: Vercel Serverless dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 
 import path from 'path';
 
@@ -9,6 +10,11 @@ import path from 'path';
 export default defineConfig({
   integrations: [react()],
   output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  }),
   vite: {
     plugins: [tailwindcss()],
     resolve: {
