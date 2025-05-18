fa701234d7a0ce2886838e2c70159439c3b567dd
RVSolutionsPlus <rvsolutionsplus@Mac.lan>
2025-05-18 14:45:12 -0500

    feat: removing vercel files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+
 import path from 'path';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  }),
-  // Configuración para resolver correctamente archivos JavaScript
   vite: {
     plugins: [tailwindcss()],
     resolve: {
